c5462b91824cb08fd84c7b70b383c1d66ea52009
Elvira Fuente <hebebicho@gmail.com>
2019-05-21 11:44:06 +0200

    add favorites state and star render

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -3,13 +3,38 @@ import Pokemon from './Pokemon';
 import '../components/pokelist.css';
 
 class PokeList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            favorites: [],
+        };
+        this.handlerClickCard = this.handlerClickCard.bind(this);
+        // this.handlerFavorites = this.handlerFavorites.bind(this);
+    }
+
+    handlerClickCard(event){
+        const targetID = parseInt(event.currentTarget.id);
+        
+        this.setState(prevState => {
+            if(this.state.favorites.includes(targetID)){
+                return {favorites: prevState.favorites.filter(item => item !== targetID)}
+            }else {
+                return {favorites: prevState.favorites.concat(targetID)}
+            }
+            
+        })
+
+    }
+
+ 
+
     render() {
         const pokemons = this.props.data
         return (
             <ul className="Pokemon__list">
                 {pokemons.map(item =>
-                    <li key={item.id} className="Pokemon__card">
-                        <Pokemon data={item} key={item.id} />
+                    <li key={item.id}>
+                        <Pokemon data={item} key={`${item.id}`} method={this.handlerClickCard} favorites={this.state.favorites}/>
                     </li>
                 )}
             </ul>
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,28 +6,32 @@ class Pokemon extends React.Component {
   render() {
     const item = this.props.data
     console.log(item)
+    const {favorites} =this.props;
 
     return (
-      <React.Fragment>
+      <div className="Pokemon__card" data-id={item.id}>
         <img src={item.url} alt={item.name} className="card__img" />
-        <h2 className="card__title">{item.name} </h2>
+        <div  className="card__title">
+          <h2>{item.name} </h2>
+          <i className={`${favorites.includes(item.id) ? `fas` : `far`} fa-star favorite__pokemon`} id={`${item.id}`} onClick={this.props.method}></i>
+        </div>
         <ul className="card__info">
           {item.types.map((typeTag, index) =>
             <li key={`p${index}`}>{typeTag}</li>
           )}
         </ul>
-      </React.Fragment>
+      </div>
     )
   }
 }
 
-Pokemon.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    types: PropTypes.array
-  }).isRequired,
-}
+// Pokemon.propTypes = {
+//   data: PropTypes.shape({
+//     id: PropTypes.number,
+//     name: PropTypes.string,
+//     types: PropTypes.array
+//   }).isRequired,
+// }
 
 
 export default Pokemon
